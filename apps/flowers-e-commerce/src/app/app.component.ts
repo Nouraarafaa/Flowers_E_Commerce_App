b57a3456a053cabd42af3150617cd416677402ac
Noura Arafa <160177373+Nouraarafaa@users.noreply.github.com>
2025-10-24 15:26:36 +0300

    Added reusable inputs (#6)
    * Added reusable inputs
    
    * feat/add validation and fixed comments
    
    * fix/comments
    
    * fix/comments

--- apps/flowers-e-commerce/src/app/app.component.ts
+++ apps/flowers-e-commerce/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { InputsComponent } from "./shared/inputs/inputs.component";
+import { LoginComponent } from "./features/auth/login/login.component";
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, InputsComponent, LoginComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'flowers-e-commerce';
+
+  
 }
