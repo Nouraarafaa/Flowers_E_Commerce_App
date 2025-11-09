f77aed0948295dbbc9d556f5834b1106f18e0c5e
Noura Arafa <nouraarafa180@gmail.com>
2025-11-09 17:46:21 +0200

    Trusted By Section is done

--- apps/flowers-e-commerce/src/app/Features/pages/home/home.component.ts
+++ apps/flowers-e-commerce/src/app/Features/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TrustedByComponent } from "../homeComponents/TrustedBy/TrustedBy.component";
+import { GalleryComponent } from "../homeComponents/gallery/gallery.component";
 
 @Component({
   selector: 'app-home',
-  imports: [TrustedByComponent],
+  imports: [TrustedByComponent, GalleryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
