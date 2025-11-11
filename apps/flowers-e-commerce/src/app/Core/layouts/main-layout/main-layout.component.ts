2b15a3e5bd02d034911b3f5f160cc088f9cd6555
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2025-11-11 12:30:43 +0300

    Recovered stashed local changes before pull

--- apps/flowers-e-commerce/src/app/Core/layouts/main-layout/main-layout.component.ts
+++ apps/flowers-e-commerce/src/app/Core/layouts/main-layout/main-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from "@angular/router";
+import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule],
+  imports: [RouterModule, NavbarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
---
+++ apps/flowers-e-commerce/src/app/Shared/components/nav-icon/navIcon.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-nav-icon',
+  imports: [RouterLink],
+  templateUrl: './navIcon.component.html',
+  styleUrl: './navIcon.component.scss',
+})
+export class NavIconComponent {
+  pageName =input.required<string>();
+  iconName=input.required<string>();
+  itemsCheck=input.required<number>();
+}
