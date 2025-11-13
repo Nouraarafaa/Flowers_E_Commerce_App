370398453f49a6a68a60486cb7fbf8b119ac7838
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2025-11-13 14:13:08 +0300

    added navigation bar

--- apps/flowers-e-commerce/src/app/Core/layouts/main-layout/main-layout.component.ts
+++ apps/flowers-e-commerce/src/app/Core/layouts/main-layout/main-layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 import { NavSideBarComponent } from "../../../shared/components/nav-side-bar/navSideBar.component";
+import { NavigationBarComponent } from "../../../shared/components/navigation-bar/navigationBar.component";
 
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule, NavbarComponent, NavSideBarComponent],
+  imports: [RouterModule, NavbarComponent, NavSideBarComponent, NavigationBarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
