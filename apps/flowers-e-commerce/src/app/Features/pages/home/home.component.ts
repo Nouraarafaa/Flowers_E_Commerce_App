d0c1249564ef6de1e9ebeb946132b3c6f48abe7b
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2025-11-15 12:56:02 +0300

    added benefits section

--- apps/flowers-e-commerce/src/app/Features/pages/home/home.component.ts
+++ apps/flowers-e-commerce/src/app/Features/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TrustedByComponent } from "../homeComponents/TrustedBy/TrustedBy.component";
 import { GalleryComponent } from "../homeComponents/gallery/gallery.component";
+import { BenefitsComponent } from '../homeComponents/benefits/benefits.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TrustedByComponent, GalleryComponent],
-  templateUrl: './home.component.html',
+  imports: [TrustedByComponent, GalleryComponent,BenefitsComponent],
+  templateUrl:'./home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {}
