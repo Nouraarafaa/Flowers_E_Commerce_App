ca63c70d06c0e0d8a1f4f73282d62f0f5c3e5fd1
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2025-11-16 11:57:50 +0300

    Amendments in html main layout file

--- apps/flowers-e-commerce/src/app/Core/layouts/main-layout/main-layout.component.ts
+++ apps/flowers-e-commerce/src/app/Core/layouts/main-layout/main-layout.component.ts
@@ -2,7 +2,6 @@ import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 import { NavSideBarComponent } from "../../../shared/components/nav-side-bar/navSideBar.component";
-import { NavigationBarComponent } from "../../../shared/components/navigation-bar/navigationBar.component";
 import { map, Subscription } from 'rxjs';
 import { AuthService } from '@elevate-workspace/auth';
 import { LocationAdaptorService } from '../../adaptor/location-adaptor/location-adaptor.service';
@@ -13,7 +12,7 @@ import { locationResponse } from '../../interfaces/location/location.response';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule, NavbarComponent, NavSideBarComponent, NavigationBarComponent],
+  imports: [RouterModule, NavbarComponent, NavSideBarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
