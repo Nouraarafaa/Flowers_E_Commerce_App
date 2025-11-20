5f9af561b00cc34b25a6e6ac3c37a37ab83d704f
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2025-11-20 08:08:39 +0300

    added hover effect to navigation link

--- apps/flowers-e-commerce/src/app/Shared/components/navigation-bar/navigationBar.component.ts
+++ apps/flowers-e-commerce/src/app/Shared/components/navigation-bar/navigationBar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
-import { NavigationLinkComponent } from "../navigation-link/navigationLink.component";
+import { NavigationLinkComponent } from '../navigation-link/navigationLink.component';
+
 
 @Component({
   selector: 'app-navigation-bar',
-  imports: [RouterLinkActive, RouterLink, NavigationLinkComponent],
+  imports: [NavigationLinkComponent],
   templateUrl: './navigationBar.component.html',
   styleUrl: './navigationBar.component.scss',
 })
