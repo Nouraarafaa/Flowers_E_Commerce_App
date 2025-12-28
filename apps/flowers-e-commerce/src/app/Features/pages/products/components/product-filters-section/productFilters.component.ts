d7f9caa5bd60bf2364db7dcb0de7df32b5c799dd
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2025-12-28 14:52:59 +0300

    resolved conflicts

--- apps/flowers-e-commerce/src/app/Features/pages/products/components/product-filters-section/productFilters.component.ts
+++ apps/flowers-e-commerce/src/app/Features/pages/products/components/product-filters-section/productFilters.component.ts
@@ -1,8 +1,5 @@
 import { Component, inject, input, signal } from '@angular/core';
-import {
-  Category,
-  Occasion,
-} from 'apps/flowers-e-commerce/src/app/Shared/interfaces/HomeResponse/home-response';
+import {Category,Occasion,} from 'apps/flowers-e-commerce/src/app/Shared/interfaces/HomeResponse/home-response';
 import { FilterNameComponent } from '../filter-name/filterName.component';
 import { SlicePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -10,7 +7,6 @@ import { Rating } from 'primeng/rating';
 import { Store } from '@ngrx/store';
 import * as ProductActions from 'apps/flowers-e-commerce/src/app/Core/store/products/products.actions';
 import { Slider } from 'primeng/slider';
-
 
 @Component({
   selector: 'app-product-filters',
@@ -25,7 +21,7 @@ export class ProductFiltersComponent {
   selectedCategoryIds = signal<string[]>([]);
   selectedOccasionIds = signal<string[]>([]);
 
-  rangeValues: number[] = [0, 0];
+  rangeValues: number[] = [0, 100];
 
   starsNumsSelected: number=0;
 
@@ -33,7 +29,7 @@ export class ProductFiltersComponent {
 
   /* ================= CATEGORY ================= */
   filterByCategory(category: Category) {
-    this.selectedCategoryIds.update(currentIds => {
+    this.selectedCategoryIds.update((currentIds) => {
       const id = category._id;
       if (currentIds.includes(id)) {
         // If ID is already present, remove it (deselect)
@@ -54,16 +50,7 @@ export class ProductFiltersComponent {
         return [...currentIds, id];
       }
     });
-
-    this._store.dispatch(
-      ProductActions.setFilters({
-        filters: {
-          category: this.selectedCategoryIds().length
-            ? this.selectedCategoryIds()
-            : null,
-        },
-      })
-    );
+    // console.log(this.selectedCategoryIds());
   }
 
   filterByOccasion(occasion: Occasion) {
@@ -96,46 +83,24 @@ export class ProductFiltersComponent {
       ProductActions.setFilters({
         filters: {
           minPrice: this.rangeValues[0],
-          maxPrice: this.rangeValues[1]
-        }
-      })
-    );
-    this._store.dispatch(
-      ProductActions.setFilters({
-        filters: {
-          minPrice: this.rangeValues[0],
-          maxPrice: this.rangeValues[1]
-        }
-      })
-    );
-    this._store.dispatch(
-      ProductActions.setFilters({
-        filters: {
-          minPrice: this.rangeValues[0],
           maxPrice: this.rangeValues[1],
         },
+      })
+    );
+    this._store.dispatch(
+      ProductActions.setFilters({
+        filters: {
+          minPrice: this.rangeValues[0],
+          maxPrice: this.rangeValues[1]
+        }
       })
     );
   }
 
   convertRangeToNumber(index: 0 | 1) {
-    let value = this.rangeValues[index];
+    const value = this.rangeValues[index];
 
 
-    if (typeof value === 'string') {
-      let numericValue = parseFloat(value);
-
-      // 1. Check bounds against [min] and [max] (2000 in your case)
-      if (numericValue < 0) numericValue = 0;
-      if (numericValue > 2000) numericValue = 2000;
-
-      if (!isNaN(numericValue)) {
-        // 2. Update the value in the existing array
-        this.rangeValues[index] = numericValue;
-
-        // 3. CRITICAL STEP: Replace the array with a new copy.
-        // This forces Angular and the p-slider component to re-render.
-        this.rangeValues = [...this.rangeValues];
     if (typeof value === 'string') {
       let numericValue = parseFloat(value);
 
