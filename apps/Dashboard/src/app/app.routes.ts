df1d29052c26b84ebc7e8a96808927eeb846326e
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2026-02-13 04:59:28 +0300

    added admin guard

--- apps/Dashboard/src/app/app.routes.ts
+++ apps/Dashboard/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Route } from '@angular/router';
+import { adminGuard } from './core/guards/admin-guard/admin.guard';
 
 export const appRoutes: Route[] = [
     {
-     path: "", loadComponent: () => import('./core/layout/main-layout/main-layout-component/mainLayout.component').then((c) => c.MainLayoutComponent), children: [
+     path: "", loadComponent: () => import('./core/layout/main-layout/main-layout-component/mainLayout.component').then((c) => c.MainLayoutComponent),canActivate: [adminGuard], children: [
     { path: "", redirectTo: "dashboard", pathMatch: "full" },
     { path: "dashboard", loadComponent: () => import('./features/dashboard/components/dashboard-component/dashboard.component').then((c) => c.DashboardComponent), title: "Dashboard" },
     { path: "categories", loadComponent: () => import('./features/categories/components/categories-component/categories.component').then((c) => c.CategoriesComponent), title: "Categories" },
