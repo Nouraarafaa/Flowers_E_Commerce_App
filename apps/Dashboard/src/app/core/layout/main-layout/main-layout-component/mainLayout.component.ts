50a5eb7b1742ef2e7ec5a8ce19d6ec79829e8271
Mohamed-Ahmed530 <mohamed.ahmed.mmaa.530@gmail.com>
2026-03-02 23:30:55 +0200

    Add: Mobile Navbar feature

--- apps/Dashboard/src/app/core/layout/main-layout/main-layout-component/mainLayout.component.ts
+++ apps/Dashboard/src/app/core/layout/main-layout/main-layout-component/mainLayout.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SideBarComponent } from "../components/side-bar/components/side-bar-component/sideBar.component";
-import { RouterModule } from "@angular/router";
-
+import { Router, RouterModule } from "@angular/router";
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-main-layout',
-  imports: [SideBarComponent, RouterModule],
+  imports: [SideBarComponent, RouterModule, Menu, ButtonModule],
   templateUrl: './mainLayout.component.html',
   styleUrl: './mainLayout.component.scss',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent implements OnInit {
+
+  private readonly _router = inject(Router);
+
+    items: MenuItem[] | undefined;
+
+    ngOnInit() {
+      this.items = [
+          {
+              label: 'name',
+              items: [
+                  {
+                      label: 'Profile',
+                      icon: 'pi pi-user',
+                      command:() => {
+                        this._router.navigate(["/update-profile"])
+                      },
+                  },
+                  {
+                      label: 'Log Out', 
+                      icon: 'pi pi-sign-out',
+                      command:() => {
+                        this.logOut();
+                      },
+                  }
+              ]
+          }
+      ];
+    }
+
+
+    logOut() {
+      console.log("Log Out");
+    }
+
+
+}
