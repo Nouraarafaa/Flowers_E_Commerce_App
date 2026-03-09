5777fa5a8e34c281160f7763121ca03b28a90e23
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2026-03-09 20:55:38 +0300

    implemented delete occassion

--- apps/Dashboard/src/app/features/occassions/components/occasions-component/occassions.component.ts
+++ apps/Dashboard/src/app/features/occassions/components/occasions-component/occassions.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { OccassionService } from '../../services/occassion.service';
+import { Subject, takeUntil } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-occassions',
@@ -7,4 +10,22 @@ import { RouterLink } from '@angular/router';
   templateUrl: './occassions.component.html',
   styleUrl: './occassions.component.scss',
 })
-export class OccassionsComponent {}
+export class OccassionsComponent implements OnDestroy{
+  private readonly _occassionService=inject(OccassionService);
+  private readonly _toastrService=inject(ToastrService);
+
+  destroy$ = new Subject<void>();
+  
+  deleteOccasion(id:string){
+    this._occassionService.deleteOccassion(id).pipe(takeUntil(this.destroy$)).subscribe({
+      next:()=>{
+        this._toastrService.success('Occasion deleted successfully');
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete(); 
+  }
+}
