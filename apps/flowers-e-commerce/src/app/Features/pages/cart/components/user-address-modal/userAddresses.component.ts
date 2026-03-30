b8e8a4c0831ea0c9f92b994a0963458838c9fe2c
Asmaa Magdy <asmaa.magdy8921@gmail.com>
2026-03-31 01:21:11 +0300

    Feature/add delete address popup (#88)
    * Recovered stashed local changes before pull
    
    * retrive navbar code
    
    * Applied stashed changes and finalized feature components
    
    * adding navbar feature with some modification
    
    * added navigation bar
    
    * Editing the navigation bar on the small screen
    
    * added section name and title
    
    * added benefits section
    
    * edit the font family
    
    * Amendments in nav sidebar
    
    * saving code
    
    * added navigation bar to main layout
    
    * added hover effect to navigation link
    
    * solved conflicts
    
    * resolved conflicts
    
    * added carousel
    
    * Adjusting the title length on the product card
    
    * clear the spaces
    
    * minor adjustments
    
    * display all products with filter
    
    * Comments have been resolved
    
    * create store
    
    * resolve comments
    
    * Fix netlify error
    
    * git commit removed
    
    * added filter price
    
    * sort products in effect
    
    * added price range slider
    
    * added filter by star rating
    
    * reset rating filter
    
    * reset price filter
    
    * reset all filters
    
    * resolved comments
    
    * resolved comment
    
    * resolved conflicts
    
    * delete isLoading to resolve error
    
    * minor adjustment in skeleton placeholder logic
    
    * added change password feature
    
    * remove dublicated code
    
    * create wishlist page
    
    * design wishlist item
    
    * resolved comments
    
    * resolve conflicts & added whishlist routing that removed
    
    * view Cart page
    
    * navigate to checkout steps
    
    * edit the function name
    
    * view all saved address
    
    * minor edit
    
    * minor edit
    
    * added ui for new address modal
    
    * added new address form
    
    * added new address feature
    
    * added dark theme to the modal
    
    * minor edit
    
    * implement side bar
    
    * main layout added
    
    * added navigation to website
    
    * implement restrict access to admin dashboard
    
    * added admin guard
    
    * update profile
    
    * updated personal info and profile image feature implemented
    
    * minor edit
    
    * editin user name ,last name , email and photo instantly in side bar when changed in profile page
    
    * added cookies
    
    * add category feature
    
    * edit component name
    
    * resolved errors
    
    * Categories table (#79)
    
    * create dashboard project in apps
    
    * feat(dashboard): implement occasions, products, and categories pages
    
    * feat(dashboard): implement Unauthorized Access, Page Not Available, and Network Error handling
    
    * resolved some issues
    
    * seve changes
    
    * edit not-auth feature
    
    * added popup
    
    ---------
    
    Co-authored-by: Noura Arafa <nouraarafa180@gmail.com>
    Co-authored-by: Noura Arafa <160177373+Nouraarafaa@users.noreply.github.com>

--- apps/flowers-e-commerce/src/app/Features/pages/cart/components/user-address-modal/userAddresses.component.ts
+++ apps/flowers-e-commerce/src/app/Features/pages/cart/components/user-address-modal/userAddresses.component.ts
@@ -1,9 +1,13 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { Address } from '../../interfaces/address';
+import { ConfirmationService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+
 
 @Component({
   selector: 'app-user-addresses',
-  imports: [],
+  imports: [ConfirmDialogModule],
+  providers: [ConfirmationService],
   templateUrl: './userAddresses.component.html',
   styleUrl: './userAddresses.component.scss',
 })
@@ -13,6 +17,9 @@ export class UserAddressesComponent {
   addAddressClicked = output<void>();
   editAddressClicked = output<Address>();
   deleteAddressClicked = output<string>();
+  deleteAddressConfirmed = output<boolean>();
+  closeModal = output<boolean>();
+  confirmationService = inject(ConfirmationService);
 
   onAddNewAddress() {
     this.addAddressClicked.emit();
@@ -20,8 +27,37 @@ export class UserAddressesComponent {
   onEditAddress(address: Address) {
     this.editAddressClicked.emit(address);
   }
+  
   onDeleteAddress(addressId: string) {
     this.deleteAddressClicked.emit(addressId);
+    this.deleteAddressConfirmed.emit(true);
   }
+
+   confirmDelete(event: Event, id:string) {
+      this.confirmationService.confirm({
+          key: 'globalConfirm',
+          target: event.target as EventTarget,
+          message: 'Are you sure you want to delete this address?',
+          header: '',
+          icon: '',
+          rejectLabel: 'Cancel',
+          closable: true,
+          closeOnEscape: true,
+          dismissableMask: true,
+          rejectButtonProps: {
+              label: 'Cancel',
+              severity: 'secondary',
+              outlined: true,
+          },
+          acceptButtonProps: {
+              label: 'Confirm',
+              severity: 'danger',
+          },
+          accept: () => {
+            this.onDeleteAddress(id);
+          }
+      });
+  }
+  
 
 }
